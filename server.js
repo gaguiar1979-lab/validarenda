f65f77504055a5298a36a9a797af88a66ec18ba3
gaguiar1979-lab <gaguiar1979@gmail.com>
2026-05-08 18:56:15 -0300

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const path = require('path');
 
 const app = express();
 app.use(express.json({ limit: '50mb' }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '.')));
 
 const ANTHROPIC_KEY = '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';
 
